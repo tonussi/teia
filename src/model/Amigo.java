210a480458d8f0b81777877e61c21330704a8be9
tonussi <lptonussi@gmail.com>
2014-04-12 14:37:44 -0300

    Add abstracao amigo

---
+++ src/model/Amigo.java
@@ -0,0 +1,30 @@
+package model;
+
+import processing.core.PApplet;
+import processing.core.PVector;
+
+public abstract class Amigo {
+
+	protected Info info;
+	protected PVector local, dir, acel;
+	PApplet processing;
+
+	public Amigo(PApplet processing, Info info) {
+
+		this.processing = processing;
+		this.info = info;
+
+		local = new PVector(processing.random(processing.width),
+				processing.random(processing.height));
+		dir = PVector.random2D();
+		acel = PVector.random2D();
+	}
+
+	public void mova() {
+		local.add(dir);
+		local.add(acel);
+	}
+
+	public void display() {
+	}
+}
