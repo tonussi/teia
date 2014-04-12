88714e4b55cb3f6fa7315c1b8f18015fa50a5b9a
tonussi <lptonussi@gmail.com>
2014-04-12 19:39:37 -0300

    Refatora impl dbconnection

--- src/dao/DBConnectionImpl.java
+++ src/dao/DBConnectionImpl.java
@@ -2,7 +2,9 @@ package dao;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -10,21 +12,46 @@ public class DBConnectionImpl implements DBConnection {
 
   Logger logger = Logger.getLogger(DBConnectionImpl.class.getName());
 
-  private String database, user, pass;
+  private final String database, user, pass;
+
+  private Connection connection;
+
+  private Statement statement;
+
+  private ResultSet resultSet;
 
   public DBConnectionImpl(String database, String user, String pass) {
+
     this.database = database;
     this.user = user;
     this.pass = pass;
+
+    verifica();
+
+  }
+
+  @Override
+  public void verifica() {
+    try {
+      connection = connect();
+      statement = connection.createStatement();
+      resultSet = statement.executeQuery("SELECT VERSION()");
+      if (resultSet.next())
+        System.out.println(resultSet.getString(1));
+    } catch (SQLException event) {
+      logger.log(Level.SEVERE, event.getMessage(), event);
+    } finally {
+      close();
+    }
   }
 
   @Override
   public Connection connect() {
-    Connection conn = null;
+    Connection connection = null;
     try {
       Class.forName("org.postgresql.Driver");
       String url = "jdbc:postgresql://localhost/" + database;
-      conn = DriverManager.getConnection(url, user, pass);
+      connection = DriverManager.getConnection(url, user, pass);
     } catch (ClassNotFoundException event) {
       logger.log(Level.SEVERE, event.getMessage(), event);
       System.exit(1);
@@ -32,11 +59,20 @@ public class DBConnectionImpl implements DBConnection {
       logger.log(Level.SEVERE, event.getMessage(), event);
       System.exit(2);
     }
-    return conn;
+    return connection;
   }
 
   @Override
-  public Connection close() {
-    return null;
+  public void close() {
+    try {
+      if (resultSet != null)
+        resultSet.close();
+      if (statement != null)
+        statement.close();
+      if (connection != null)
+        connection.close();
+    } catch (SQLException event) {
+      logger.log(Level.WARNING, event.getMessage(), event);
+    }
   }
 }
