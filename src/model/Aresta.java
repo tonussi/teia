71602f169f81c9f8a60b05c6b7b757b635dbf5ac
tonussi <lptonussi@gmail.com>
2014-04-12 14:37:10 -0300

    Add aresta grafo

---
+++ src/model/Aresta.java
@@ -0,0 +1,46 @@
+package model;
+
+import processing.core.PApplet;
+
+public class Aresta {
+	private int comprimento;
+	private int largura;
+	private int forca;
+	PApplet parent;
+
+	public Aresta(PApplet p, int comprimento, int largura, int forca) {
+		super();
+
+		parent = p;
+		this.comprimento = comprimento;
+		this.largura = largura;
+		this.forca = forca;
+	}
+
+	public void display() {
+	}
+
+	public int getComprimento() {
+		return comprimento;
+	}
+
+	public void setComprimento(int comprimento) {
+		this.comprimento = comprimento;
+	}
+
+	public int getLargura() {
+		return largura;
+	}
+
+	public void setLargura(int largura) {
+		this.largura = largura;
+	}
+
+	public int getForca() {
+		return forca;
+	}
+
+	public void setForca(int forca) {
+		this.forca = forca;
+	}
+}
