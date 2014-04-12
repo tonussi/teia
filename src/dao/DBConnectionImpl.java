b65dc719f4ad4d4c9e4720e332085024e137410d
tonussi <lptonussi@gmail.com>
2014-04-12 14:38:00 -0300

    Add db connetor impl

---
+++ src/dao/DBConnectionImpl.java
@@ -0,0 +1,38 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class DBConnectionImpl implements DBConnection {
+
+	Logger logger = Logger.getLogger(DBConnectionImpl.class
+			.getName());
+
+	private String database, user, pass;
+
+	public DBConnectionImpl(String database, String user, String pass) {
+		this.database = database;
+		this.user = user;
+		this.pass = pass;
+	}
+
+	@Override
+	public Connection connectToDatabaseOrDie() {
+		Connection conn = null;
+		try {
+			Class.forName("org.postgresql.Driver");
+			String url = "jdbc:postgresql://localhost/" + database;
+			conn = DriverManager.getConnection(url, user, pass);
+		} catch (ClassNotFoundException event) {
+			logger.log(Level.SEVERE, event.getMessage(), event);
+			System.exit(1);
+		} catch (SQLException event) {
+			logger.log(Level.SEVERE, event.getMessage(), event);
+			System.exit(2);
+		}
+		return conn;
+	}
+}
