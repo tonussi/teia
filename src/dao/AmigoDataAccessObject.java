8e5637f81c5253106fb3e448c40ffe0b9f3b57a6
tonussi <lptonussi@gmail.com>
2014-04-12 14:38:33 -0300

    Add dao interface

---
+++ src/dao/AmigoDataAccessObject.java
@@ -0,0 +1,18 @@
+package dao;
+
+import java.util.List;
+
+import model.Info;
+import model.Nodo;
+
+public interface AmigoDataAccessObject {
+
+	public int quantidadeAmigos();
+
+	public void comparaTabelas();
+
+	public List<Info> listadeAmigos(String sex);
+
+	public List<Nodo> listadeRelacoes();
+
+}
