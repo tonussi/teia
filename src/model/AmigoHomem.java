eccef25176fb8a1933ecc933e31e581cedf5bb5a
tonussi <lptonussi@gmail.com>
2014-04-12 14:37:36 -0300

    Add especializacao tipo homem

---
+++ src/model/AmigoHomem.java
@@ -0,0 +1,31 @@
+package model;
+
+import processing.core.PApplet;
+
+public class AmigoHomem extends Amigo {
+
+	public AmigoHomem(PApplet processing, Info info) {
+		super(processing, info);
+	}
+
+	@Override
+	public void display() {
+		processing.pushMatrix();
+		processing.translate(local.x, local.y);
+
+		processing.fill(255);
+
+		processing.triangle(local.x, local.y + 3, local.x + 3, local.y,
+				local.x - 3, local.y);
+
+		processing.stroke(255);
+		processing.strokeWeight(2);
+		processing.noFill();
+
+		processing.ellipse(local.x, local.y, 20, 20);
+
+		info.adicionaInformacao(local.x, local.y);
+
+		processing.popMatrix();
+	}
+}
