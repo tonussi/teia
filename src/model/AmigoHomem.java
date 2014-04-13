5c01c499a31281d7774409a3ad3f45f484b07789
tonussi <lptonussi@gmail.com>
2014-04-13 17:05:05 -0300

    Adiciona comentario explicando o desenho

--- src/model/AmigoHomem.java
+++ src/model/AmigoHomem.java
@@ -13,15 +13,33 @@ public class AmigoHomem extends Amigo {
     processing.pushMatrix();
     processing.translate(local.x, local.y);
 
+    /*
+     * Desenho: Triangulo
+     * 
+     * Preenchimento: Branco
+     * 
+     * Contorno: Branco
+     * 
+     * Peso do Contorno: 1 pixel wide
+     */
     processing.fill(255);
-    processing.noStroke();
-    processing.triangle(local.x, local.y + 3, local.x + 3, local.y,
-        local.x - 3, local.y);
+    processing.stroke(255);
+    processing.strokeWeight(1);
+    processing.triangle(local.x, local.y - 3, local.x - 3, local.y,
+        local.x + 3, local.y);
 
-    processing.stroke(88, 32, 244);
-    processing.strokeWeight(2);
+    /*
+     * Desenho: Circulo
+     * 
+     * Preenchimento: Vazio
+     * 
+     * Contorno: Azul Escuro
+     * 
+     * Peso do Contorno: 2 pixel wide
+     */
     processing.noFill();
-
+    processing.strokeWeight(2);
+    processing.stroke(88, 32, 244);
     processing.ellipse(local.x, local.y, 20, 20);
 
     info.adicionaInformacao(local.x, local.y);
