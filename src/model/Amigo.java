fa900102ffd8723cedc3925e0132ae6f35134f36
tonussi <lptonussi@gmail.com>
2014-04-14 13:31:28 -0300

    Amigo contem apenas info

--- src/model/Amigo.java
+++ src/model/Amigo.java
@@ -1,30 +1,11 @@
 package model;
 
-import processing.core.PApplet;
-import processing.core.PVector;
-
 public abstract class Amigo {
 
   protected Info info;
-  protected PVector local, dir, acel;
-  PApplet processing;
 
-  public Amigo(PApplet processing, Info info) {
-
-    this.processing = processing;
+  public Amigo(Info info) {
     this.info = info;
-
-    local = new PVector(processing.random(processing.width),
-        processing.random(processing.height));
-    dir = PVector.random2D();
-    acel = PVector.random2D();
   }
-
-  public void mova() {
-    local.add(dir);
-    local.add(acel);
-  }
-
-  public abstract void display();
 
 }
