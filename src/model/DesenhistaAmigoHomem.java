1ce3d0140d73aec2ee72b70a7f72713990d27389
tonussi <lptonussi@gmail.com>
2014-04-14 23:12:29 -0300

    Implementa metodo escreve para desenhadores tipo homem

--- src/model/DesenhistaAmigoHomem.java
+++ src/model/DesenhistaAmigoHomem.java
@@ -1,67 +1,77 @@
 package model;
 
 import processing.core.PApplet;
+import processing.core.PConstants;
+import processing.core.PFont;
 
 public class DesenhistaAmigoHomem implements Desenhador, Movedor {
 
-  VetorComposto vetoresMovimento;
+  VetorComposto vetorComposto;
   AmigoHomem amigoHomem;
   PApplet processing;
+  PFont font;
 
-  public DesenhistaAmigoHomem(PApplet processing, AmigoHomem amigoHomem) {
+  public DesenhistaAmigoHomem(PApplet processing, PFont font,
+      AmigoHomem amigoHomem) {
 
+    this.font = font;
     this.amigoHomem = amigoHomem;
     this.processing = processing;
-    this.vetoresMovimento = new VetorComposto();
+    this.vetorComposto = new VetorComposto();
 
   }
 
   @Override
   public void display() {
     processing.pushMatrix();
-    processing.translate(vetoresMovimento.local.x, vetoresMovimento.local.y);
+    processing.translate(vetorComposto.local.x, vetorComposto.local.y);
 
     /*
      * Desenho: Triangulo
-     *
+     * 
      * Preenchimento: Branco
-     *
+     * 
      * Contorno: Branco
-     *
+     * 
      * Peso do Contorno: 1 pixel wide
      */
     processing.fill(255);
     processing.stroke(255);
     processing.strokeWeight(1);
-    processing.triangle(vetoresMovimento.local.x, vetoresMovimento.local.y - 3,
-        vetoresMovimento.local.x - 3, vetoresMovimento.local.y,
-        vetoresMovimento.local.x + 3, vetoresMovimento.local.y);
+    processing.triangle(vetorComposto.local.x, vetorComposto.local.y + 3,
+        vetorComposto.local.x - 3, vetorComposto.local.y,
+        vetorComposto.local.x + 3, vetorComposto.local.y);
 
     /*
      * Desenho: Circulo
-     *
+     * 
      * Preenchimento: Vazio
-     *
+     * 
      * Contorno: Azul Escuro
-     *
+     * 
      * Peso do Contorno: 2 pixel wide
      */
     processing.noFill();
     processing.strokeWeight(2);
     processing.stroke(88, 32, 244);
-    processing.ellipse(vetoresMovimento.local.x, vetoresMovimento.local.y, 20,
-        20);
-
-    amigoHomem.info.adicionaInformacao(vetoresMovimento.local.x,
-        vetoresMovimento.local.y);
+    processing.ellipse(vetorComposto.local.x, vetorComposto.local.y, 20, 20);
 
     processing.popMatrix();
   }
 
   @Override
   public void mova() {
-    vetoresMovimento.local.add(vetoresMovimento.dir);
-    vetoresMovimento.local.add(vetoresMovimento.acel);
+    vetorComposto.local.add(vetorComposto.dir);
+    vetorComposto.local.add(vetorComposto.acel);
+  }
+
+  @Override
+  public void escreve() {
+    processing.fill(255, 60);
+    processing.textAlign(PConstants.LEFT);
+    processing.textFont(font);
+    processing.text(toString(), vetorComposto.local.x + 12,
+        vetorComposto.local.y + 12);
   }
 
 }
