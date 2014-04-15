01e9fdfb93431864640d1a06edc14e5af290c136
tonussi <lptonussi@gmail.com>
2014-04-14 21:24:45 -0300

    Deixa VetorComposto simples por enquanto

---
+++ src/model/VetorComposto.java
@@ -0,0 +1,17 @@
+package model;
+
+import processing.core.PVector;
+
+public class VetorComposto {
+
+  PVector local, dir, acel;
+
+  public VetorComposto() {
+
+    local = PVector.random2D();
+    dir = PVector.random2D();
+    acel = PVector.random2D();
+
+  }
+
+}
