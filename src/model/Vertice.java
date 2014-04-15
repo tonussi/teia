864557a1791f06a15fb7f9908462f6daba3e1539
tonussi <lptonussi@gmail.com>
2014-04-14 23:15:14 -0300

    Remove papplet dessa classe

--- src/model/Vertice.java
+++ src/model/Vertice.java
@@ -5,21 +5,17 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import processing.core.PApplet;
-
 public class Vertice {
 
   private final BigInteger identificador;
   private final Map<BigInteger, List<BigInteger>> mapeamentoNodular;
-  PApplet processing;
 
-  public Vertice(PApplet processing, BigInteger identificador,
-      List<BigInteger> mapeamentoNodular) {
+  public Vertice(BigInteger identificador, List<BigInteger> mapeamentoNodular) {
 
-    this.processing = processing;
     this.identificador = identificador;
 
     this.mapeamentoNodular = new HashMap<BigInteger, List<BigInteger>>();
+
     this.mapeamentoNodular.put(identificador, mapeamentoNodular);
 
   }
