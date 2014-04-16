a82a1e5ba547ea8879a07a2add381782b61db353
tonussi <lptonussi@gmail.com>
2014-04-15 21:25:51 -0300

    Implementacao do especialista em movimento

---
+++ src/view/EspecialistaMovimento.java
@@ -0,0 +1,13 @@
+package view;
+
+
+
+public class EspecialistaMovimento implements Movedor {
+
+  @Override
+  public void combina(VetorComposto vetorComposto) {
+    vetorComposto.local.add(vetorComposto.dir);
+    vetorComposto.local.add(vetorComposto.acel);
+  }
+
+}
