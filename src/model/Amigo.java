5323577376a46d5c0d63f1cfafe2a071d7c606ce
tonussi <lptonussi@gmail.com>
2014-04-15 21:18:46 -0300

    Adiciona vetor composto ao amigo

--- src/model/Amigo.java
+++ src/model/Amigo.java
@@ -1,11 +1,25 @@
 package model;
 
+import view.VetorComposto;
+
 public abstract class Amigo {
 
+  VetorComposto vetorComposto;
   protected Info info;
 
-  public Amigo(Info info) {
+  public Amigo(Info info, VetorComposto vetorComposto) {
+
     this.info = info;
+    this.vetorComposto = vetorComposto;
+
+  }
+
+  public Info getInfo() {
+    return info;
+  }
+
+  public VetorComposto getVetorComposto() {
+    return vetorComposto;
   }
 
 }
