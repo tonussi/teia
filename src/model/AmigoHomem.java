1adf5d8a22bd1c41c006d37a5a7f1fa16b56296c
tonussi <lptonussi@gmail.com>
2014-04-15 21:19:13 -0300

    Adiciona vetor composto ao amigo tipo homem

--- src/model/AmigoHomem.java
+++ src/model/AmigoHomem.java
@@ -1,9 +1,13 @@
 package model;
 
+import view.VetorComposto;
+
 public class AmigoHomem extends Amigo {
 
-  public AmigoHomem(Info info) {
-    super(info);
+  public AmigoHomem(Info info, VetorComposto vetorComposto) {
+
+    super(info, vetorComposto);
+
   }
 
 }
