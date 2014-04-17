a1017646842e6e5e7d30ca2f9336ed4e1fc8f796
tonussi <lptonussi@gmail.com>
2014-04-17 19:42:37 -0300

    Adiciona thread trabalhadora de amigos destacados

---
+++ src/view/TrabalhadorAmigoDestacado.java
@@ -0,0 +1,28 @@
+package view;
+
+import java.util.List;
+
+import model.AmigoDestacado;
+
+public class TrabalhadorAmigoDestacado extends Thread {
+
+  List<AmigoDestacado> amigosHomens;
+  DesenhadorEspecializado especialistaDesenho;
+  Movedor especialistaMovimento;
+
+  public TrabalhadorAmigoDestacado(List<AmigoDestacado> amigosHomens,
+      DesenhadorEspecializado especialistaDesenho, Movedor especialistaMovimento) {
+    this.amigosHomens = amigosHomens;
+    this.especialistaDesenho = especialistaDesenho;
+    this.especialistaMovimento = especialistaMovimento;
+  }
+
+  @Override
+  public void run() {
+    for (AmigoDestacado amigoDestacado : amigosHomens) {
+      especialistaDesenho.escrevaInfoAmigoDestacado(amigoDestacado);
+      especialistaDesenho.displayDestacado(amigoDestacado);
+      especialistaMovimento.combina(amigoDestacado.getVetorComposto());
+    }
+  }
+}
