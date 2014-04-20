e7c1c3cac42cf8565fa1d6e6193c130717323102
tonussi <lptonussi@gmail.com>
2014-04-20 19:49:34 -0300

    Change name of the interface to Desenhador

---
+++ src/view/Desenhador.java
@@ -0,0 +1,21 @@
+package view;
+
+import model.AmigoDestacado;
+import model.AmigoHomem;
+import model.AmigoMulher;
+
+public interface Desenhador {
+
+  public void displayHomem(AmigoHomem amigoHomem);
+
+  public void displayMulher(AmigoMulher amigoMulher);
+
+  public void escrevaInfoAmigoHomem(AmigoHomem amigoHomem);
+
+  public void escrevaInfoAmigoMulher(AmigoMulher amigoMulher);
+
+  public void escrevaInfoAmigoDestacado(AmigoDestacado amigoDestacado);
+
+  public void displayDestacado(AmigoDestacado amigoDestacado);
+
+}
