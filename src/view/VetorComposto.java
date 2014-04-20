9362c7fdce54a9dbaed5acb25689b1cee2001575
tonussi <lptonussi@gmail.com>
2014-04-20 19:54:50 -0300

    Adiciona nova combinacao de vetores

--- src/view/VetorComposto.java
+++ src/view/VetorComposto.java
@@ -4,15 +4,13 @@ import processing.core.PVector;
 
 public class VetorComposto {
 
-  PVector local, dir, acel;
+  PVector local, dir, vel;
 
   public VetorComposto() {
 
-    local = new PVector(0, 0);
-
     dir = PVector.random2D();
-
-    acel = new PVector(34e-3F, 12e-2F);
+    local = new PVector(0, 0);
+    vel = new PVector(0.9182682f, 0.39595902f, 0.0f);
 
   }
 
