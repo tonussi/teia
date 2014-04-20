26a18e6be5ac232a436fd554c07f56c7a9d3b92f
tonussi <lptonussi@gmail.com>
2014-04-20 19:52:58 -0300

    Implementa novos metodos para formacao geometrica organizada

--- src/view/EspecialistaMovimento.java
+++ src/view/EspecialistaMovimento.java
@@ -1,13 +1,33 @@
 package view;
 
+import processing.core.PVector;
+
 public class EspecialistaMovimento implements Movedor {
 
   @Override
-  public void combina(VetorComposto vetorComposto) {
+  public void combina(VetorComposto vets) {
 
-    vetorComposto.local.add(vetorComposto.dir);
-    vetorComposto.local.add(vetorComposto.acel);
+    vets.local.add(vets.dir);
+    vets.local.add(vets.vel);
 
+    vets.local.limit(300);
+
+  }
+
+  @Override
+  public void formacaoNumeroUm(VetorComposto vets) {
+
+    vets.local.add(vets.dir);
+    vets.local.limit(90);
+    vets.local.mult(angulo(vets));
+    vets.local.rotate(12.6f);
+    vets.dir.sub(vets.local);
+
+  }
+
+  @Override
+  public float angulo(VetorComposto vets) {
+    return -PVector.angleBetween(vets.dir, vets.vel);
   }
 
 }
